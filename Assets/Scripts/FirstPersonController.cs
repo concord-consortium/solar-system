62944e06742e7d29883fb6f2a00b41c583dcbc97
Eli Kosminsky <ekosminsky@concord.org>
2017-10-05 20:21:57 -0700

    Basic movement on a globe

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+    public float mouseSensitivityX = 3.5f;
+    public float mouseSensitivityY = 3.5f;
+    public float walkSpeed = 4;
+
+    Transform cameraT;
+    float verticalLookRotation;
+
+    Vector3 moveAmount;
+    Vector3 smoothVelocity;
+    Rigidbody myBody;
+
+	void Start () {
+        cameraT = Camera.main.transform;
+        myBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+
+        verticalLookRotation += Input.GetAxis(("Mouse Y")) * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60, 60);
+        cameraT.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothVelocity, .15f);
+	}
+
+    private void FixedUpdate() {
+        myBody.MovePosition(myBody.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/GravityBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour {
+
+    GravityAttractor planet;
+    Rigidbody myBody;
+
+    void Awake() {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+
+        myBody = GetComponent<Rigidbody>();
+        myBody.useGravity = false;
+        myBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void FixedUpdate() {
+        planet.Attract(transform);
+    }
+}
