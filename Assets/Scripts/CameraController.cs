62b392570e3f213eeb139eed9c821449598787d6
Eli Kosminsky <ekosminsky@concord.org>
2017-10-06 23:19:23 -0700

    Adding camera switching

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public RenderTexture viewTexture;
+    public GameObject antView;
+    public GameObject personView;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.C)) {
+            if (antView.tag == "MainCamera") {
+                antView.tag = "Untagged";
+                personView.tag = "MainCamera";
+
+                personView.SetActive(true);
+                antView.GetComponent<Camera>().targetTexture = viewTexture;
+			} else {
+                antView.tag = "MainCamera";
+                personView.tag = "Untagged";
+
+                personView.SetActive(false);
+                antView.GetComponent<Camera>().targetTexture = null;
+            }
+		}
+	}
+}
