2075faec1d54417c13b5f3f5d47b3824b8a13617
ekosminsky <ekosminsky@concord.org>
2017-10-10 16:54:37 -0700

    Adding VR to scene, with laser pointer controllers

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public RenderTexture viewTexture;
-    public GameObject antView;
-    public GameObject personView;
+  public RenderTexture viewTexture;
+  public GameObject antView;
+  public GameObject personView;
 
-    public GameObject ant;
-    public GameObject planet;
+  public GameObject ant;
+  public GameObject planet;
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.C)) {
-            if (antView.tag == "MainCamera") {
-                // Switch to person view
-                antView.tag = "Untagged";
-                personView.tag = "MainCamera";
+  void Start() {
+    if (UnityEngine.VR.VRDevice.isPresent) {
+      antView.SetActive(false);
+    }
+  }
 
-                personView.SetActive(true);
-                antView.GetComponent<Camera>().targetTexture = viewTexture;
+  // Update is called once per frame
+  void Update () {
+    if (!UnityEngine.VR.VRDevice.isPresent) {
+      if (Input.GetKeyDown(KeyCode.C)) {
+        if (antView.tag == "MainCamera") {
+          // Switch to person view
+          antView.tag = "Untagged";
+          personView.tag = "MainCamera";
 
-                ant.transform.parent = planet.transform;
-			} else {
-                // Switch to ant view
-                antView.tag = "MainCamera";
-                personView.tag = "Untagged";
+          personView.SetActive(true);
+          antView.GetComponent<Camera>().targetTexture = viewTexture;
 
-                personView.SetActive(false);
-                antView.GetComponent<Camera>().targetTexture = null;
+          ant.transform.parent = planet.transform;
+        } else {
+          // Switch to ant view
+          antView.tag = "MainCamera";
+          personView.tag = "Untagged";
 
-                ant.transform.parent = null;
-            }
-		}
+          personView.SetActive(false);
+          antView.GetComponent<Camera>().targetTexture = null;
+
+          ant.transform.parent = null;
+        }
+      }
+    }
 	}
 }
--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -22,15 +22,15 @@ public class FirstPersonController : MonoBehaviour {
 	
 	void Update () {
         if (Camera.main.name == "AntCamera") {
-			transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+			    transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
 
-			verticalLookRotation += Input.GetAxis(("Mouse Y")) * mouseSensitivityY;
-			verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90, 90);
-			cameraT.localEulerAngles = Vector3.left * verticalLookRotation;
+			    verticalLookRotation += Input.GetAxis(("Mouse Y")) * mouseSensitivityY;
+			    verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90, 90);
+			    cameraT.localEulerAngles = Vector3.left * verticalLookRotation;
 
-			Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-			Vector3 targetMoveAmount = moveDir * walkSpeed;
-			moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothVelocity, .15f);
+			    Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+			    Vector3 targetMoveAmount = moveDir * walkSpeed;
+			    moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothVelocity, .15f);
         }
 	}
 
