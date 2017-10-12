c75e0397edcba71214fc9af7259653eccc0b0920
ekosminsky <ekosminsky@concord.org>
2017-10-11 19:23:33 -0700

    Moving beach ball with controller

---
+++ Assets/Scripts/BeachBallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBallMovement : MonoBehaviour {
+
+  public GameObject beachBall;
+  public GameObject personView;
+
+  private SteamVR_TrackedObject trackedObj;
+  private SteamVR_Controller.Device Controller {
+    get { return SteamVR_Controller.Input((int)trackedObj.index); }
+  }
+
+  void Awake() {
+    trackedObj = GetComponent<SteamVR_TrackedObject>();
+  }
+	
+	// Update is called once per frame
+	void Update() {
+    if (personView.tag == "MainCamera") {
+      beachBall.transform.rotation = trackedObj.transform.rotation;
+      beachBall.transform.Rotate(90, 0, 0);
+    }
+	}
+}
